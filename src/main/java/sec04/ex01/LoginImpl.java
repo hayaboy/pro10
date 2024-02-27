d3837eb583d9308ed5c7024c44b0ad77f1ca8a6e
hayaboy <hayaboy@korea.ac.kr>
2024-02-27 11:43:52 +0900

    2_27_pro10, 인코딩 필터와 세션 이벤트

---
+++ src/main/java/sec04/ex01/LoginImpl.java
@@ -0,0 +1,44 @@
+package sec04.ex01;
+
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+
+//HttpSessionBindingListener 세션에 바인딩되거나 바인딩 해제될 때 개체에 알림이 표시되도록 합니다. 
+
+public class LoginImpl implements HttpSessionBindingListener {
+	
+	String user_id;
+	String user_pw;
+	static int total_user=0;
+	
+	
+	public LoginImpl() {
+	System.out.println("LoginImpl 객체 만들어짐");
+	}
+	
+	
+	
+	
+	
+	public LoginImpl(String user_id, String user_pw) {
+		super();
+		this.user_id = user_id;
+		this.user_pw = user_pw;
+	}
+
+
+
+
+
+	@Override
+	public void valueBound(HttpSessionBindingEvent event) {
+		System.out.println("사용자 접속");
+		++LoginImpl.total_user;
+	}
+
+	@Override
+	public void valueUnbound(HttpSessionBindingEvent event) {
+		System.out.println("사용자 접속");
+		total_user--;
+	}
+}
---
+++ src/main/java/sec04/ex01/LoginTest2.java
@@ -0,0 +1,43 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*@WebServlet("/login")*/
+public class LoginTest2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	
+protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	PrintWriter pw = response.getWriter();
+	
+	String user_id=request.getParameter("user_id");
+	String user_pw=request.getParameter("user_pw");
+	System.out.println(user_id +user_pw );
+	
+	
+	LoginImpl loginUser= new LoginImpl(user_id,user_pw );
+	
+//	pw.write("<h6>hihihi</h6>");
+	pw.write(loginUser.user_id + "접속합");
+	pw.write("현재 접속자 수 " + LoginImpl.total_user + "명");
+//	pw.write("<h1>안녕</h1>");
+	
+	}
+}
